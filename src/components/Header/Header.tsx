cc33821cd8330b9d785f04472ae9c1d8cc9b5bef
alexandreoliv29 <alexandre.foj@gmail.com>
2023-10-01 22:31:01 -0300

    adding changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,51 +6,60 @@ export const Header = () => {
             <Navbar />
             <div className="d-flex container mt-5">
                 <div className="d-column mt-5">
-                    <p style={{ color: '#FFF', fontSize: 30, fontWeight: 600 }}>Garanta seu ID de Especiaslita e seja ÚNICO{'\n'}no mercado financeiro!</p>
-                    <p style={{ color: '#29BCBA', fontSize: 16, fontWeight: 600 }}>MAIOR ECOSSISTEMA PARA ESPECIALISTAS DO PAÍS</p>
+                    <p className="fw-bold" style={{ color: '#FFF', fontSize: 30 }}>Garanta seu ID de Especiaslita e seja ÚNICO{'\n'}no mercado financeiro!</p>
+                    <p className="fw-bold" style={{ color: '#29BCBA', fontSize: 16 }}>MAIOR ECOSSISTEMA PARA ESPECIALISTAS DO PAÍS</p>
                 </div>
                 <img width={300} height={300} src="https://advisor-walkr-bucket.s3.amazonaws.com/CircleWalkr.svg" alt="Circle Walkr" />
             </div>
 
             <div className="d-flex text-center mt-5">
-                <p style={{ color: '#FFF', fontSize: 20, }}>A Tecnologia que transforma a sua relação com o cliente!</p>
+                <p style={{ color: '#FFF', fontSize: 20 }}>A TECNOLOGIA QUE TRANSFORMA A SUA RELAÇÃO COM O CLIENTE!</p>
             </div>
 
-            <div className="d-flex justify-content-between">
-                <div className="card rounded-start" style={{ backgroundColor: '#29BCBA', height: 230 }}>
-                    <div className="logo">
-                        <img width={172} height={34} src="https://advisor-walkr-bucket.s3.amazonaws.com/Logo+WalkrAdvisor.svg" alt="Logo WalkrAdvisor" />
-                    </div>
-                    <div className="icon">
-                        <img width={50} height={50} src="https://advisor-walkr-bucket.s3.amazonaws.com/ChartIcon.svg" alt="ChartIcon" />
-                    </div>
-                    <div className="text">
-                        <p className="text-white" style={{ fontSize: 16 }}>Crie seu ID de Especialista grátis e compartilhe com os seus clientes.</p>
-                    </div>
-                </div>
-                <div className="card rounded-start" style={{ backgroundColor: 'transparent', height: 230 }}>
-                    <div className="logo">
-                        <img width={172} height={34} src="https://advisor-walkr-bucket.s3.amazonaws.com/WalkrBankBranco.svg" alt="Logo WalkrBank" />
-                    </div>
-                    <div className="icon">
-                        <img width={50} height={50} src="https://advisor-walkr-bucket.s3.amazonaws.com/WalletIcon.svg" alt="WalletIcon" />
-                    </div>
-                    <div className="text">
-                        <p className="text-white" style={{ fontSize: 16 }}>Acesse as vantagens do Banco feito para os Especialistas do Mercado Financeiro.</p>
+            <div className="d-flex justify-content-around pb-5">
+                <div className="card" style={{ width: '380px', height: '230px' }}>
+                    <div className="card-body d-column align-items-center">
+                        <div className="logo text-center" style={{ flex: 1 }}>
+                            <img width={172} height={34} src="https://advisor-walkr-bucket.s3.amazonaws.com/Logo+WalkrAdvisor.svg" alt="Logo WalkrAdvisor" />
+                        </div>
+                        <div className="icon" style={{ flex: 1 }}>
+                            <img width={50} height={50} src="https://advisor-walkr-bucket.s3.amazonaws.com/ChartIcon.svg" alt="ChartIcon" />
+                        </div>
+                        <div className="text" style={{ flex: 3 }}>
+                            <p className="text-white" style={{ fontSize: '16px', textAlign: 'left' }}>Crie seu ID de Especialista grátis e compartilhe com os seus clientes.</p>
+                        </div>
                     </div>
                 </div>
-                <div className="card rounded-start" style={{ backgroundColor: 'transparent', height: 230 }}>
-                    <div className="logo">
-                        <img width={172} height={34} src="https://advisor-walkr-bucket.s3.amazonaws.com/Logo+WalkrAdvisor.svg" alt="Logo WalkrAdvisor" />
+
+                <div className="card" style={{ width: '380px', height: '230px' }}>
+                    <div className="card-body d-column align-items-center">
+                        <div className="logo text-center" style={{ flex: 1 }}>
+                            <img width={172} height={34} src="https://advisor-walkr-bucket.s3.amazonaws.com/WalkrBankBranco.svg" alt="Logo WalkrBank" />
+                        </div>
+                        <div className="icon" style={{ flex: 1 }}>
+                            <img width={50} height={50} src="https://advisor-walkr-bucket.s3.amazonaws.com/WalletIcon.svg" alt="WalletIcon" />
+                        </div>
+                        <div className="text" style={{ flex: 3 }}>
+                            <p className="text-white" style={{ fontSize: '16px', textAlign: 'left' }}>Acesse as vantagens do Banco feito para os Especialistas do Mercado Financeiro.</p>
+                        </div>
                     </div>
-                    <div className="icon">
-                        <img width={50} height={50} src="https://advisor-walkr-bucket.s3.amazonaws.com/HashtagIcon.svg" alt="HashtagIcon" />
-                    </div>
-                    <div className="text">
-                        <p className="text-white" style={{ fontSize: 16 }}>Tenha em mãos o projeto de vida financeiro dos seus clientes.</p>
+                </div>
+
+                <div className="card" style={{ width: '380px', height: '230px' }}>
+                    <div className="card-body d-column align-items-center">
+                        <div className="logo text-center" style={{ flex: 1 }}>
+                            <img width={172} height={34} src="https://advisor-walkr-bucket.s3.amazonaws.com/Logo+WalkrAdvisor.svg" alt="Logo WalkrAdvisor" />
+                        </div>
+                        <div className="icon" style={{ flex: 1 }}>
+                            <img width={50} height={50} src="https://advisor-walkr-bucket.s3.amazonaws.com/HashtagIcon.svg" alt="HashtagIcon" />
+                        </div>
+                        <div className="text" style={{ flex: 3 }}>
+                            <p className="text-white" style={{ fontSize: '16px', textAlign: 'left' }}>Tenha em mãos o projeto de vida financeiro dos seus clientes.</p>
+                        </div>
                     </div>
                 </div>
             </div>
+
         </div>
     )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,21 @@
 export const Navbar = () => {
     return (
-        <div className="d-flex">
-            <div className="logo justify-content-start bg-danger">
-                <img src="https://advisor-walkr-bucket.s3.amazonaws.com/WalkrLogoLP.svg" width={58} height={24} alt="Logo Walkr" />
-            </div>
-            <div className="itens d-flex">
+        <div className="d-flex justify-content-between align-items-center">
+            <img className="d-flex justify-content-start" src="https://advisor-walkr-bucket.s3.amazonaws.com/WalkrLogoLP.svg" width={58} height={24} alt="Logo Walkr" />
+            <div className="itens d-flex justify-content-center">
                 <p className="text-white pt-3" style={{ fontSize: 14 }}>Home</p>
                 <p className="text-white pt-3" style={{ fontSize: 14 }}>Walkr Bank</p>
                 <p className="text-white pt-3" style={{ fontSize: 14 }}>WalkrApp</p>
                 <p className="text-white pt-3" style={{ fontSize: 14 }}>Planos</p>
                 <p className="text-white pt-3" style={{ fontSize: 14 }}>FAQ's</p>
             </div>
-            <div className="buttons d-flex pt-3 pe-5">
+            <div className="buttons d-flex">
                 <button className="btn btn-transparent text-white" style={{ fontSize: 14 }}>Cadastrar-se</button>
                 <button type="button" className="btn btn-light">Login</button>
             </div>
         </div>
+
+
+
     )
 }
