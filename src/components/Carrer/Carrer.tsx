218a312cb726acc0ec26d926c0d4ea983e1ed0e2
alexandreoliv29 <alexandre.foj@gmail.com>
2023-10-01 22:03:58 -0300

    backup

---
+++ src/components/Carrer/Carrer.tsx
@@ -0,0 +1,36 @@
+
+
+export const Carrer = () => {
+    return (
+        <div style={{ backgroundColor: '#e7e9ed' }}>
+            <div className="d-flex container">
+                <p className="text-center fw-bold pt-5" style={{ fontSize: 48, color: '#033C59' }}>Transforme sua CARREIRA em apenas 4 passsos</p>
+            </div>
+
+            <div className="d-flex justify-content-center container gap-5">
+                <div className="d-column">
+                    <img src="https://walkr-bucket.s3.amazonaws.com/FirstPathComplete.svg" alt="" />
+                    <p className="">Você se cadastra <strong>sem custos, </strong>gera seu <strong>ID de Especialista EXCLUSIVO </strong>para compartilhar com prospects e clientes</p>
+                </div>
+                <div className="d-column">
+                    <p>Você poderá ter também o <strong>Walkr Bank </strong>e aderir ao plano <strong>Advisor Premium </strong>obtendo benefícios únicos</p>
+                    <img src="https://walkr-bucket.s3.amazonaws.com/SecondPathComplete.svg" alt="" />
+                </div>
+                <div className="logo">
+                    <img src="https://walkr-bucket.s3.amazonaws.com/Logo+WA.svg" alt="" />
+                </div>
+                <div className="d-column">
+                    <p>O cliente final <strong>vincula seu ID de Especialista </strong>e você passa a ter acesso aos dados financeiros, bem como o <strong>Relatório GOLD em PDF </strong>
+                        com todo o projeto de vida do seu cliente, oferecendo <strong>soluções precisas e individualizadas</strong></p>
+                    <img src="https://walkr-bucket.s3.amazonaws.com/ThirdPathComplete.svg" alt="" />
+                </div>
+                <div className="d-column">
+                    <img src="https://walkr-bucket.s3.amazonaws.com/FourthPathComplete.svg" alt="" />
+                    <p><strong>Indique </strong>para seu cliente o <strong>Relatório GOLD</strong>em PDF dentro do WalkrApp, com um pagamento único e receba <strong>40% de comissão</strong></p>
+                </div>
+            </div>
+x
+
+        </div>
+    )
+}
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,20 @@
 export const Navbar = () => {
     return (
-        <div className="container d-flex justify-content-between align-items-center pt-3">
-            <img src="https://advisor-walkr-bucket.s3.amazonaws.com/WalkrLogoLP.svg" width={58} height={24} alt="Logo Walkr" />
-            <p className="text-white" style={{ fontSize: 14 }}>Home</p>
-            <p className="text-white" style={{ fontSize: 14 }}>Walkr Bank</p>
-            <p className="text-white" style={{ fontSize: 14 }}>WalkrApp</p>
-            <p className="text-white" style={{ fontSize: 14 }}>Planos</p>
-            <p className="text-white" style={{ fontSize: 14 }}>FAQ's</p>
-            <button className="btn btn-transparent text-white" style={{ fontSize: 14 }}>Cadastrar-se</button>
-            <button type="button" className="btn btn-light">Login</button>
+        <div className="d-flex">
+            <div className="logo justify-content-start bg-danger">
+                <img src="https://advisor-walkr-bucket.s3.amazonaws.com/WalkrLogoLP.svg" width={58} height={24} alt="Logo Walkr" />
+            </div>
+            <div className="itens d-flex">
+                <p className="text-white pt-3" style={{ fontSize: 14 }}>Home</p>
+                <p className="text-white pt-3" style={{ fontSize: 14 }}>Walkr Bank</p>
+                <p className="text-white pt-3" style={{ fontSize: 14 }}>WalkrApp</p>
+                <p className="text-white pt-3" style={{ fontSize: 14 }}>Planos</p>
+                <p className="text-white pt-3" style={{ fontSize: 14 }}>FAQ's</p>
+            </div>
+            <div className="buttons d-flex pt-3 pe-5">
+                <button className="btn btn-transparent text-white" style={{ fontSize: 14 }}>Cadastrar-se</button>
+                <button type="button" className="btn btn-light">Login</button>
+            </div>
         </div>
-
     )
 }
